c933f7614fde71e0af017926af60016ccb5ece26
Moustapha <amroune.moustapha@gmail.com>
2025-04-30 10:57:57 +0200

    add markdown component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
 import './App.css';
+import ReactMarkdown from 'react-markdown';
+import { useState } from 'react';
 
 function App() {
+  const [markdownText, setMarkdownText] = useState("");
   return (
     <div className="App">
-   
+      <h1>Markdown Previewer</h1>
+      <div className="boxes-container" >
+        <textarea name="editor" id="editor" ></textarea>
+        <div id="preview" >
+          <ReactMarkdown>{markdownText}</ReactMarkdown>
+        </div>
+      
+      </div>
     </div>
   );
 }
