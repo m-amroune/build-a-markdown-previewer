c4c8101be0989cab616ff4ac76f24ff0e0490ae6
Moustapha <amroune.moustapha@gmail.com>
2025-04-30 12:09:19 +0200

    add content for markdown

--- src/App.js
+++ src/App.js
@@ -3,13 +3,47 @@ import './App.css';
 import ReactMarkdown from 'react-markdown';
 import { useState } from 'react';
 
+const defaultMarkdown = `
+# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... **_both!_**
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+
+
+- And of course there are lists.
+  - Some are bulleted.
+      - With different indentation levels.
+        - That look like this.
+`;
+
+
 function App() {
-  const [markdownText, setMarkdownText] = useState("");
+  const [markdownText, setMarkdownText] = useState(defaultMarkdown);
   return (
     <div className="App">
       <h1>Markdown Previewer</h1>
       <div className="boxes-container" >
-        <textarea name="editor" id="editor" ></textarea>
+        <textarea name="editor" id="editor" value={markdownText}></textarea>
         <div id="preview" >
           <ReactMarkdown>{markdownText}</ReactMarkdown>
         </div>
